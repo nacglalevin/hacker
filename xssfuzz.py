166838c6ac1451a45b372e7f0765a87e4065ba44
Lalevin <167055481+nacglalevin@users.noreply.github.com>
2024-05-02 22:50:30 +0800

    更新 xssfuzz.py

--- xssfuzz.py
+++ xssfuzz.py
@@ -1,5 +1,5 @@
 """
-==========================================
+========================================
 Name:Centurion Author: Lalevin Martin
  Mailbox: zzlyxht@outlook.com                                                
  Github: http://github.com/nacglalevin
